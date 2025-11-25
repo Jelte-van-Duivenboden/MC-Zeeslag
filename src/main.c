8fea23c12339732defd438245ebf0ba574008e33
Carmilla-1213394 <s1213394@student.windesheim.nl>
2025-11-25 10:06:22 +0100

    Knop en LED verbonden

--- src/main.c
+++ src/main.c
@@ -16,10 +16,27 @@ int main()
   TIMSK0 |= (1<<OCIE0A);
   sei();
   DDRD |= (1<<DDD6);
+  DDRB |= (1<<DDB3) | (1<<DDB4);
 
   while(1)
   {
-    
+    if (~(PINB | ~(1<<PINB3)))
+    {
+      DDRD |= (1<<DDD6);
+    }
+    else
+    {
+      DDRD &= ~(1<<DDD6);
+    }
+
+    if (~(PIND | ~(1<<PIND2)))
+    {
+      PORTB &= ~(1<<PORTB4);
+    }
+    else
+    {
+      PORTB |= (1<<PORTB4);
+    }
   }
 
   return 0;
